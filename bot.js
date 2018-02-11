840f19161b0f007eec39646fabe776a2a34dcf15
Unknown <pablo.bgc@gmail.com>
2018-02-11 21:12:42 +0100

    Wololo_bot v0.9
    almost completed bot

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
-const botSettings = require("./botSettings.json")
+const botSettings = require("./botSettings.json");
+const ytdl = require('ytdl-core');
 const bot = new Discord.Client( {disableEveryone: true} );
 const prefix= botSettings.prefix;
 
@@ -9,20 +10,43 @@ bot.on('ready', async () => {
 });
 
 bot.on('message', async message => {
-  if (message.content === 'ping') {
-    message.reply('Puto CIShetero de mierda!!!! chupame la regla');
-  }
 
-  if(!message.content.startsWith(prefix)) return;
-  if(message.content === `${prefix}userInfo`){
+  if(message.author.bot) return;
+  if (message.content === `${prefix}wololeave`){
+  		message.member.voiceChannel.leave();
+  }
+  if (message.content === `${prefix}wololo`){
   	let embed = new Discord.RichEmbed()
   		.setAuthor(message.author.username)
-  		.setDescription("This is the user info!!");
+  		.setDescription("Mande?");
   	message.channel.send(embed);
+  	const connection = await message.member.voiceChannel.join();
+  	const dispatcher = connection.playStream(ytdl(`https://www.youtube.com/watch?v=lKQBTuXEWo0`, { filter : 'audioonly' }), {
+  		volume: 0.5
+		});
+  	setWololo(true);
+  	
   }
+  if(wololo){
+  	if(message.content.match('\\d{1,2}(?!\\d)|100')){ 
+  			const connection = await message.member.voiceChannel.join(); 		
+  			const dispatcher = connection.playFile(`./sounds/${message.content}.mp3`, {
+  			volume: 0.5
+			});
+  	
+  	
+	}
+}
 
+
+  
 
 });
 
 bot.login(botSettings.token);
+
+var wololo=false;
+function setWololo(w){
+	wololo=w;
+}
 
